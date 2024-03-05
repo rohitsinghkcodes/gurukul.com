f983d79167edc9aa6bf5baea284184239df57e10
rohit <mailsrohitk@gmail.com>
2024-03-05 16:51:15 +0530

    checkpoint multer start

--- client/src/Pages/ResearchPaperViewer.js
+++ client/src/Pages/ResearchPaperViewer.js
@@ -1,228 +1,88 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FaDownload } from "react-icons/fa6";
+import Layout from "../Components/Layouts/Layout";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const YouTubePlayerPage = () => {
+  const params = useParams();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [paperData, setPaperData] = useState([]);
+
+  const getAllPaperData = async () => {
+    try {
+      const { data } = await axios.get(
+        `/api/v1/papers/get-single-paper/${params.slug}`
+      );
+      if (data?.success) {
+        setPaperData(data.paper);
+      }
+    } catch (error) {
+      toast.error("Something went wrong while fethcing research papers!");
+    }
+  };
+  
+
+  useEffect(() => {
+    getAllPaperData();
+    //eslint-disable-next-line
+  }, []);
   return (
-    <div className="m-5">
-      <div className="card rounded-5 bg-transparent text-light">
-        <div className="row ">
-          <div className="col-md-5">
-            <img
-              src={`https://images.everydayhealth.com/images/diet-nutrition/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1`}
-              className="rounded-5 img-fluid"
-              style={{ width: "600px", maxHeight: "600px" }}
-            />
-          </div>
-          <div className="col-md-7">
-            <div className="card-body">
-              <h5
-                className="card-title"
-                style={{
-                  fontSize: "54px",
-                }}
-              >
-                7787
-              </h5>
-              <p className="card-text">
-                878878888888888888888888888888888888888888888888888888888888888888888888888888888888888888888lorem
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
-                quas corrupti atque expedita nulla error voluptate natus id
-                suscipit excepturi eos sapiente necessitatibus praesentium,
-                incidunt, impedit reiciendis quod quos pariatur!
-              </p>
-              <h6
-                className="card-title"
-                style={{
-                  fontSize: "28px",
-                }}
-              >
-                <span
+    <Layout>
+      <div className="m-5">
+        <div className="card rounded-5 bg-transparent text-light">
+          <div className="row ">
+            <div className="col-md-5">
+              <img
+                src={`https://images.everydayhealth.com/images/diet-nutrition/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1`}
+                className="rounded-5 img-fluid"
+                style={{ width: "600px", maxHeight: "600px" }}
+              />
+            </div>
+            <div className="col-md-7">
+              <div className="card-body">
+                <h5
                   style={{
-                    fontSize: "13px",
-                    color: "#a9a9a9",
-                    verticalAlign: "super",
+                    fontSize: "35px",
+                    color: "#8a2be2",
                   }}
                 >
-                  ₹
-                </span>
-                2424242
-                <span
-                  className="ms-1"
-                  style={{ fontSize: "14px", color: "#a9a9a9" }}
-                >
-                  MRP:{" "}
-                  <span
-                    style={{
-                      textDecoration: "line-through",
-                      fontSize: "13px",
-                      color: "#a9a9a9",
-                    }}
-                  >
-                    ₹242424
-                  </span>
-                  <span
-                    className="ms-1"
-                    style={{
-                      fontSize: "18px",
-                      color: "rgb(204, 204, 42) ",
-                    }}
-                  >
-                    % off
-                  </span>
-                </span>
-              </h6>
-
-              {/* <div className="row text-center bg-transparent mt-4">
-                  <div className="col-md-2">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <FaTruckFast
-                        size="30px"
-                        color="wheat"
-                        style={{
-                          transform: "rotateY(180deg)", // Fix the rotate property
-                        }}
-                      />
-                      <p
-                        className="m-0 mt-2" // Add margin to separate the text from the icon
-                        style={{
-                          fontSize: "12px",
-                        }}
-                      >
-                        Free Delivery
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-2 ">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <IoMdCash size="30px" color="wheat" />
-                      <p
-                        className="m-0 mt-2" // Add margin to separate the text from the icon
-                        style={{
-                          fontSize: "12px",
-                        }}
-                      >
-                        Pay on Delivery
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="col-md-2">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <TbTruckReturn size="30px" color="wheat" />
-
-                      <p
-                        className="m-0 mt-2" // Add margin to separate the text from the icon
-                        style={{
-                          fontSize: "12px",
-                        }}
-                      >
-                        10 days replacement
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="col-md-2">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <IoIosMedal size="30px" color="wheat" />
-
-                      <p
-                        className="m-0 mt-2" // Add margin to separate the text from the icon
-                        style={{
-                          fontSize: "12px",
-                          marginTop: "4px",
-                        }}
-                      >
-                        Top Brand
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <RiSecurePaymentLine size="30px" color="wheat" />
-
-                      <p
-                        className="m-0 mt-2" // Add margin to separate the text from the icon
-                        style={{
-                          fontSize: "12px",
-                          marginTop: "4px",
-                        }}
-                      >
-                        Secure Transaction
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div
-                      className="container pt-3 m-2 rounded-4 d-flex flex-column align-items-center"
-                      style={{
-                        height: "100px",
-                        backgroundColor: "rgba(65, 65, 65, 0.285)",
-                      }}
-                    >
-                      <div className="d-flex flex-column align-items-center">
-                        <MdOutlineAssignmentReturn size="30px" color="wheat" />
-                        <p
-                          className="m-0 mt-2"
-                          style={{ fontSize: "12px", marginTop: "4px" }}
-                        >
-                          Non-Returnable
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
-
-              <div className="text-center d-flex justify-content-between">
-                <button
-                  className="btn btn-danger mt-4 py-3 "
+                  {paperData.name}
+                </h5>
+                <p
                   style={{
-                    width: "40%",
-                    borderRadius: "20px",
+                    fontSize: "20px",
                   }}
                 >
-                  <FaDownload
-                    size="25px"
-                    style={{ justifyContent: "center", alignItems: "center" }}
-                  />{" "}
-                  &ensp; Download Paper
-                </button>
+                  {paperData.description}
+                </p>
+                <p className="text-secondary">
+                  Last Updated {paperData.pdf}
+                </p>
+
+                <div className="text-center d-flex justify-content-between">
+                  <button
+                    className="btn btn-danger mt-4 py-3 "
+                    style={{
+                      width: "40%",
+                      borderRadius: "20px",
+                    }}
+                  >
+                    <FaDownload
+                      size="25px"
+                      style={{ justifyContent: "center", alignItems: "center" }}
+                    />{" "}
+                    &ensp; Download Paper
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
--- client/src/Pages/ResearchPapers.js
+++ client/src/Pages/ResearchPapers.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Layout from "../Components/Layouts/Layout";
 import { toast } from "react-toastify";
 import axios from "axios";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 const ResearchPapers = () => {
   const [researchPapers, setResearchPapers] = useState([]);
@@ -29,33 +31,38 @@ const ResearchPapers = () => {
           <h1 className="mb-2 ms-4 ">Research Papers</h1>
           <div className="d-flex justify-content-center row mx-4 ">
             {researchPapers.map((rp) => (
-              <div
-                className="card mb-3 rounded-4 px-4"
-                style={{ minWidth: "100vh" }}
+              <Link
                 key={rp._id}
+                to={`/research-paper-viewer/${rp.slug}`}
+                className="product-link"
               >
-                <div className="row g-0">
-                  <div className="col-md-2 py-4">
-                    {/* TODO: ADD PDF VIEWER HERE INSTED OF IMG */}
-                    <img
-                      src="https://media.licdn.com/dms/image/D4D12AQHl1HFTvItqpw/article-cover_image-shrink_720_1280/0/1690440690219?e=2147483647&v=beta&t=fN3x7AxsHbO9VNmQS3rXS3QVymAzr5vDoTjmUMHea1w"
-                      className="img-fluid rounded-4"
-                      alt="..."
-                    />
-                  </div>
-                  <div className="col-md-9 py-3">
-                    <div className="card-body">
-                      <h5 className="card-title">{rp.name}</h5>
-                      <p className="card-text">{rp.description}</p>
-                      <p className="card-text">
-                        <small className="text-body-secondary">
-                          Last updated {rp.updatedAt}
-                        </small>
-                      </p>
+                <div
+                  className="card mb-3 rounded-4 px-4"
+                  style={{ minWidth: "100vh" }}
+                >
+                  <div className="row g-0">
+                    <div className="col-md-2 py-4">
+                      {/* TODO: ADD PDF VIEWER HERE INSTED OF IMG */}
+                      <img
+                        src="https://media.licdn.com/dms/image/D4D12AQHl1HFTvItqpw/article-cover_image-shrink_720_1280/0/1690440690219?e=2147483647&v=beta&t=fN3x7AxsHbO9VNmQS3rXS3QVymAzr5vDoTjmUMHea1w"
+                        className="img-fluid rounded-4"
+                        alt="..."
+                      />
+                    </div>
+                    <div className="col-md-9 py-3">
+                      <div className="card-body">
+                        <h5 className="card-title">{rp.name}</h5>
+                        <p className="card-text">{rp.description}</p>
+                        <p className="card-text">
+                          <small className="text-body-secondary">
+                            Last updated {moment(rp.updatedAt).fromNow()}
+                          </small>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
